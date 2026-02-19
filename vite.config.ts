fe8bf3206d8b10c8924c3f7efa36f7883dff84ef
drfoxg <drfoxg@gmail.com>
2026-02-19 15:36:16 +0300

    Конфиг для Vite Hot Module Replacement

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,32 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { fileURLToPath, URL } from "node:url";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd());
 
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
-  },
+    return {
+        plugins: [vue()],
 
-  server: {
-    host: "0.0.0.0", // чтобы контейнер был доступен снаружи
-    port: 5173, // можно любой свободный порт
-    strictPort: true, // не переключать порт автоматически
-    hmr: {
-      host: "goapp.sulfurfun.ru", // для Hot Module Replacement через внешний домен
-      protocol: "wss", // ws(HTTP) или wss(HTTPS)
-    },
-  },
+        resolve: {
+            alias: {
+                "@": fileURLToPath(new URL("./src", import.meta.url)),
+            },
+        },
 
-  build: {
-    // Показывать предупреждения о больших чанках
-    chunkSizeWarningLimit: 500,
-  },
+        server: {
+            host: "0.0.0.0", // чтобы контейнер был доступен снаружи
+            port: 5173, // можно любой свободный порт
+            strictPort: true, // не переключать порт автоматически
+            hmr: {
+                host: env.VITE_HMR_HOST || "localhost", // для Hot Module Replacement через внешний домен
+                protocol: env.VITE_HMR_PROTOCOL || "wss", // ws(HTTP) или wss(HTTPS)
+            },
+        },
+
+        build: {
+            // Показывать предупреждения о больших чанках
+            chunkSizeWarningLimit: 500,
+        },
+    };
 });
